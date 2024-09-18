32920d0e78a8751530e45ed2dab5062266f1094d
JonasGz <jonaschagasweb@gmail.com>
2024-09-18 19:13:03 -0300

    feat: create form component

---
+++ src/app/components/Form/Form.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./Form.scss";
+
+const Form = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function changeEmail(e) {
+    setEmail(e.target.value);
+  }
+  function changePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("deu submit");
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <div className="form-container__inputs">
+        <input
+          className="form-container__input"
+          type="email"
+          name="email"
+          placeholder="jonaschagasweb@gmail.com"
+          onChange={changeEmail}
+          value={email}
+        />
+        <input
+          className="form-container__input"
+          type="password"
+          name="password"
+          placeholder="*********"
+          onChange={changePassword}
+          value={password}
+        />
+      </div>
+      <div className="form-container__buttons">
+        <button className="form-container__button" type="submit">
+          Login
+        </button>
+        <span className="form-container__inner-text">
+          Do you have an account?
+        </span>
+        <button
+          className="form-container__button form-container__button--secondary"
+          type="button"
+        >
+          Sign up
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;

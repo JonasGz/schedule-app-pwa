84dfea906c3633268066c31c7ac7ddb49e961243
JonasGz <jonaschagasweb@gmail.com>
2024-09-27 21:58:51 -0300

    feat: add redirect if login and signup

--- src/app/tasks/page.js
+++ src/app/tasks/page.js
@@ -7,21 +7,24 @@ import List from "../components/List/List";
 import AddTasks from "../components/AddTasks/AddTasks";
 import Image from "next/image";
 import { useTask } from "../../../contexts/TaskContext";
+import PrivateRouter from "../components/PrivateRouter.js/PrivateRouter";
 
 export default function Tasks() {
   const { tasks, setAtt } = useTask();
 
   return (
-    <div className="tasks">
-      <Navbar title="TASKS" subtitle="Control" />
-      <div className="tasks__add-tasks">
-        <AddTasks setAtt={setAtt} />
-      </div>
-      <Image src="/images/break.png" width={91} height={26} alt="break" />
+    <PrivateRouter>
+      <div className="tasks">
+        <Navbar title="TASKS" subtitle="Control" />
+        <div className="tasks__add-tasks">
+          <AddTasks setAtt={setAtt} />
+        </div>
+        <Image src="/images/break.png" width={91} height={26} alt="break" />
 
-      <div className="tasks__tasks">
-        <List tasks={tasks} setAtt={setAtt} />
+        <div className="tasks__tasks">
+          <List tasks={tasks} setAtt={setAtt} />
+        </div>
       </div>
-    </div>
+    </PrivateRouter>
   );
 }

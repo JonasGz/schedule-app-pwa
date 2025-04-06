5afe2bf9cda1914d5c3b84bde2407db0288d63c3
Jonas Chagas <jonaschagasweb@gmail.com>
2025-04-06 16:12:16 -0300

    feat: active navigate

--- src/app/components/Navigate/Navigate.js
+++ src/app/components/Navigate/Navigate.js
@@ -3,15 +3,19 @@ import { LayoutDashboard } from 'lucide-react';
 import './Navigate.scss';
 import { useRouter } from 'next/navigation';
 import { ListTodo } from 'lucide-react';
+import { usePathname } from 'next/navigation';
 
 const Navigate = () => {
   const router = useRouter();
+  const pathname = usePathname();
+  console.log(pathname.includes('/dashboard'));
+  
 
   return(
     <div className="navigate">
       <ul className="navigate__ul">
-        <li onClick={() => router.push('/dashboard')} className="navigate__li"><LayoutDashboard size={20} />Dashboard</li>
-        <li onClick={() => router.push('/tasks')} className="navigate__li"><ListTodo size={20} />Tasks</li>
+        <li onClick={() => router.push('/dashboard')} className={`navigate__li ${pathname.includes('/dashboard') ? 'active' : ''}`} ><LayoutDashboard size={20} />Dashboard</li>
+        <li onClick={() => router.push('/tasks')} className={`navigate__li ${pathname.includes('/tasks') ? 'active' : ''}`}><ListTodo size={20} />Tasks</li>
       </ul>
     </div>
   )
